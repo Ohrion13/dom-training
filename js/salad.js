a2dc9b6e8587924d0aa400c6c621620afe50ef90
Ohrion <mortharion13@gmail.com>
2024-04-18 13:57:55 +0200

    save salad quest 1

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,20 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+
+const ingredientsList = document.getElementById('salad-ingredients');
+
+for (let i = 0; i < ingredients.length; i++) {
+    
+    const listItm = document.createElement('li');
+    
+    ingredientsList.appendChild(listItm);
+    
+    const btn = document.createElement('button');
+    
+    btn.classList.add('button');
+    
+    btn.textContent = ingredients[i];
+    
+    listItm.appendChild(btn);
+
+}
